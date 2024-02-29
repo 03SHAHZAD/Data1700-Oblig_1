2932f9f89ec99a37d7b13575bc328d017970c1a8
03SHAHZAD <113612609+03SHAHZAD@users.noreply.github.com>
2024-02-29 22:46:06 +0100

    javasskcrip update
    Funksjon for å slette billetter, Push til billetter-array. Valideringsbetingelse for telefon og et par Flere småfiks med id navn of nummer.

--- js.js
+++ js.js
@@ -2,50 +2,51 @@ let billetter=[];
 let ut="<table><tr>" +
     "<th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefon</th><th>Epost</th>"+"</tr>";
     
-    function kjopticket(){
-        const billett={
-            film: document.getElementById("Filmer").value,
-            antall: document.getElementById("Antall").value,
-            fornavn: document.getElementById("Fornavn").value,
-            etternavn: document.getElementById("Etternavn").value,
-            telefonnr: document.getElementById("Telefon").value,
-            epost: document.getElementById("Epost").value
-        };
+function kjopbilett(){
+    const billett={
+        film: document.getElementById("Filmer").value,
+        antall: document.getElementById("Antall").value,
+        fornavn: document.getElementById("Fornavn").value,
+        etternavn: document.getElementById("Etternavn").value,
+        telefonnr: document.getElementById("Telefon").value,
+        epost: document.getElementById("Epost").value
+    };
+    
+    if (!billett.antall || !billett.fornavn || !billett.etternavn || !billett.telefonnr || !billett.epost ) {
+        document.getElementById("AntallError").innerHTML="DU MÅ VELGE ANTALL!";
+        document.getElementById("FornavnError").innerHTML="DU MÅ SKRIVE UNDER ET NAVN";
+        document.getElementById("EtternavnError").innerHTML="DU MÅ SKRIVE UNDER ET NAVN!";
+        document.getElementById("TelefonError").innerHTML=" DU MANGLER TELEFON NUMMMER!";
+        document.getElementById("EpostError").innerHTML="DU MÅ SKRIVE EPOST ADRESSEN!";
+        return false; 
+     } else {
+        billetter.push(billett);
         
-        if (!billett.antall || !billett.fornavn || !billett.etternavn || !billett.telefon || !billett.epost ) {
-            document.getElementById("AntallError").innerHTML="DU MÅ VELGE ANTALL!";
-            document.getElementById("FornavnError").innerHTML="DU MÅ SKRIVE UNDER ET NAVN";
-            document.getElementById("EtternavnError").innerHTML="DU MÅ SKRIVE UNDER ET NAVN!";
-            document.getElementById("TelefonError").innerHTML=" DU MANGLER TELEFON NUMMMER!";
-            document.getElementById("EpostError").innerHTML="DU MÅ SKRIVE EPOST ADRESSEN!";
-         } else {
-            billetter.push(billett);
-            
-            for (let liste of billetter){
-                ut+="<tr>"
-                ut+="<td>" + liste.film + "</td> <td>"+liste.antall+"</td><td>"+liste.fornavn+"</td>" + "<td>" + liste.etternavn+ "</td><td>"+liste.telefonnr+"</td><td>"+liste.epost+"</td>";
-                ut+="</tr>"
-            }   
-            document.getElementById("ut").innerHTML=ut;
+        for (let liste of billetter){
+            ut+="<tr>"
+            ut+="<td>" + liste.film + "</td> <td>"+liste.antall+"</td><td>"+liste.fornavn+"</td>" + "<td>" + liste.etternavn+ "</td><td>"+liste.telefonnr+"</td><td>"+liste.epost+"</td>";
+            ut+="</tr>"
+        }   
+        document.getElementById("ut").innerHTML=ut;
 
-            billetter=[];
-    
-            document.getElementById("AntallError").innerHTML="";
-            document.getElementById("FornavnError").innerHTML="";
-            document.getElementById("EtternavnError").innerHTML="";
-            document.getElementById("TelefonError").innerHTML="";
-            document.getElementById("EpostError").innerHTML=""
-        }
-    
-        document.getElementById("Antall").value="";
-        document.getElementById("Fornavn").value="";
-        document.getElementById("Etternavn").value="";
-        document.getElementById("Telefon").value="";    
-        document.getElementById("Epost").value="";
-    
+       
+        document.getElementById("AntallError").innerHTML="";
+        document.getElementById("FornavnError").innerHTML="";
+        document.getElementById("EtternavnError").innerHTML="";
+        document.getElementById("TelefonError").innerHTML="";
+        document.getElementById("EpostError").innerHTML=""
     }
-    
-    function SlettBiletter(){
-        document.getElementById("ut").innerHTML="";
-        billetter=[];
-    }
+
+    document.getElementById("Antall").value="";
+    document.getElementById("Fornavn").value="";
+    document.getElementById("Etternavn").value="";
+    document.getElementById("Telefon").value="";    
+    document.getElementById("Epost").value="";
+
+    return false; 
+}
+
+function SlettBiletter(){
+    document.getElementById("ut").innerHTML="";
+    billetter=[];
+}
